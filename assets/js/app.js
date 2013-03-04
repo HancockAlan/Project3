ed69b787998a836c6f3e1014c7bf41ee966bb2a0
Alan Hancock <alahanco@iupui.edu>
2013-03-03 20:34:00 -0500

    Added transition capability with JS (final commit)
    Transitions have been added to move between pieces of content. This was
    achievable by use of JS to initialize the jQTouch functions. Final
    project commit.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,10 @@
 ;(function($, window) {
 	
-	$(document).ready(function() {
-		
-		$('.menuicon').click(function(e) {
-			
-			var $t = $(this);
-			var $nav = $('.nav');
-			
-			if ($t.hasClass('rotate')) {
-				$t.removeClass('rotate');
-				$nav.removeClass('show');
-			}
-			else {
-				$t.addClass('rotate');
-				$nav.addClass('show');
-			}
-			
-			e.preventDefault();
-		
-		});
-		
-		$('.type-buttons button').click(function() {
-			var $buttons = $('.type-buttons button');
-			var $t = $(this);
-			var type = $t.attr('id');
-			
-			$buttons.removeClass('primary');
-			$t.addClass('primary');
-		
-			for ( x=1; x<=3; x++ ) {
-				$('body').removeClass('type-'+x);
-			}
-			
-			if ($('.nav').hasClass('show'))
-			{
-				$('.menuicon').removeClass('rotate')
-				$('nav').removeClass('show');
-			}
-			
-			$('body').addClass(type);
-		});
+	// initialize jQTouch
+    $(function(){
 	
-	});
+	    var jQT = new $.jQTouch({ });
+
+    });
 	
 }(jQuery, this));
